79a5fa23085bd566a7801b88f964d159832654b7
nihashi000 <stereophonic.zwei@gmail.com>
2023-08-05 18:39:16 +0900

    fix protocol

--- src/const/apiUrl.ts
+++ src/const/apiUrl.ts
@@ -1,7 +1,7 @@
 import { headers } from "next/dist/client/components/headers";
 
 export const apiUrl = () => {
-  const protocol = process?.env.NODE_ENV === "development" ? "http" : "http";
+  const protocol = process?.env.NODE_ENV === "development" ? "http" : "https";
   const host = headers().get("host");
   return `${protocol}://${host}/api`;
 };
